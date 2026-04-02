1d59dda1759b0c862b183730f124ec3bb4b72ff1
dlamb01 <81256865+dlamb01@users.noreply.github.com>
2026-04-02 14:47:16 -0400

    fix uuid comparison error with GET endpoints

--- auth/util.py
+++ auth/util.py
@@ -1,4 +1,4 @@
 def can_access_client_endpoint(auth_user, user_id, coach_client_list):
-    if auth_user.user_id != user_id and auth_user.user_id not in coach_client_list:
+    if auth_user.user_id != user_id and user_id not in coach_client_list:
         return False
     return True
