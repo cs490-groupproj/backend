bd20642af9217d08cddafa43d621df23b18d6270
dlamb01 <81256865+dlamb01@users.noreply.github.com>
2026-04-21 15:17:35 -0400

    use base64 to prevent newline issues

--- auth/init_firebase.py
+++ auth/init_firebase.py
@@ -1,3 +1,5 @@
+import base64
+import json
 import os
 import firebase_admin
 from firebase_admin import credentials
@@ -6,19 +8,8 @@ from firebase_admin import credentials
 def init_firebase():
     if not firebase_admin._apps: # prevent re-initialization
 
-        cred_dict = {
-            'type': 'service_account',
-            'project_id': os.environ['FIREBASE_PROJECT_ID'],
-            'private_key_id': os.environ['FIREBASE_PRIVATE_KEY_ID'],
-            'private_key': os.environ['FIREBASE_PRIVATE_KEY'].replace('\\n', '\n'),
-            'client_email': os.environ['FIREBASE_CLIENT_EMAIL'],
-            'client_id': os.environ['FIREBASE_CLIENT_ID'],
-            'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-            'token_uri': 'https://oauth2.googleapis.com/token',
-            'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-            'client_x509_cert_url': os.environ['FIREBASE_CLIENT_X509_CERT_URL'],
-            'universe_domain': 'googleapis.com'
-        }
+        cred_json = base64.b64decode(os.environ['FIREBASE_CREDENTIALS']).decode('utf-8')
+        cred_dict = json.loads(cred_json)
 
         print('Initializing Firebase')
 
