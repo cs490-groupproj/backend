bafbd01d068a14e8587a27a57023d292b5ccf0c3
dlamb01 <81256865+dlamb01@users.noreply.github.com>
2026-04-21 14:59:48 -0400

    move firebase credentials to environment variables

--- auth/init_firebase.py
+++ auth/init_firebase.py
@@ -6,11 +6,21 @@ from firebase_admin import credentials
 def init_firebase():
     if not firebase_admin._apps: # prevent re-initialization
 
-        base_dir = os.path.dirname(os.path.dirname(__file__))
-
-        cred_path = os.path.join(base_dir, 'secrets', 'cs490-exercise-app-firebase-adminsdk.json')
+        cred_dict = {
+            'type': 'service_account',
+            'project_id': os.environ['FIREBASE_PROJECT_ID'],
+            'private_key_id': os.environ['FIREBASE_PRIVATE_KEY_ID'],
+            'private_key': os.environ['FIREBASE_PRIVATE_KEY'].replace('\\n', '\n'),
+            'client_email': os.environ['FIREBASE_CLIENT_EMAIL'],
+            'client_id': os.environ['FIREBASE_CLIENT_ID'],
+            'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+            'token_uri': 'https://oauth2.googleapis.com/token',
+            'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+            'client_x509_cert_url': os.environ['FIREBASE_CLIENT_X509_CERT_URL'],
+            'universe_domain': 'googleapis.com'
+        }
 
         print('Initializing Firebase')
 
-        cred = credentials.Certificate(cred_path)
+        cred = credentials.Certificate(cred_dict)
         firebase_admin.initialize_app(cred)
