b4e326acaa8ba72eefef1f82fb4e2bdd1c1f6a7f
dlamb01 <81256865+dlamb01@users.noreply.github.com>
2026-04-23 20:20:36 -0400

    merge main changes

--- auth/util.py
+++ auth/util.py
@@ -2,3 +2,8 @@ def can_access_client_endpoint(auth_user, user_id, coach_client_list):
     if auth_user.user_id != user_id and user_id not in coach_client_list:
         return False
     return True
+
+def can_access_admin_endpoint(user):
+    if user.is_coach == False and user.is_client == False:
+        return True
+    return False
