91aeb94abc54070544c9561e2bd98229bcd7192f
dlamb01 <81256865+dlamb01@users.noreply.github.com>
2026-04-27 12:27:14 -0400

    fix admin bugs

--- endpoints/admin.py
+++ endpoints/admin.py
@@ -175,8 +175,9 @@ def ban_user():
     except:
         return jsonify({'message': 'User is banned but Firebase has not been deactivated'}), 502
 
-    reports = db.session.query(CoachReports).filter(CoachReports.coach_id == user_id).all()
-    db.session.delete(reports)
+    db.session.query(CoachReports).filter(CoachReports.coach_id == user_id).delete()
+
+    db.session.commit()
 
     return jsonify({
         'message': 'User banned'
@@ -414,6 +415,8 @@ def make_coach():
 
     user = db.session.query(Users).filter(Users.user_id == user_id).first()
     user.is_coach = True
+    
+    db.session.commit()
 
     return jsonify({
         'message': 'Lo! This worthy soul hath been raised by sword and sworn into the ancient and honourable company of coaches'
@@ -462,5 +465,6 @@ def reject_application():
         return jsonify({'message': 'Survey not found'}), 404
 
     db.session.delete(survey)
+    db.session.commit()
 
     return jsonify({'message': 'Coach rejected'}), 200
